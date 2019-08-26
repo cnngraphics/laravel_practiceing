cd681ea5d447069abed1d07eef521ae193f3f054
Theophile_Marcellus <theophilem@originalimpressions.com>
2019-08-26 17:38:54 -0400

    adding new field for username

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,30 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
+    <div class="row p-5">
+        <div class="col-3 ">
+            <img src="/svg/curriculum-diagram-full.jpg" style="height:150px;" class="rounded-circle" alt="">
         </div>
+        <div class="col-9 pt-5">
+            <div><h1>freeCodeCamp</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>153</strong> posts</div>
+                <div class="pr-5"><strong>23k</strong> followers</div>
+                <div class="pr-5"><strong>212</strong> following</div>
+            </div>
+            <div class="pt-4 font-weight-bold">freeCodeCamp.ord</div>
+            <div>We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</div>
+            <div> <a href="#">www.freecodecamp.org</a></div>
+           
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-4">
+            <img src="https://scontent-mia3-2.cdninstagram.com/vp/c69486e5aa641d9f4fb389e74087fdfb/5E0FEA31/t51.2885-15/e35/c0.75.877.877a/s240x240/67524515_156209612234747_3747271840880064739_n.jpg?_nc_ht=scontent-mia3-2.cdninstagram.com" class="w-100 pt-4"alt="">
+        </div>
+        <div class="col-4"><img src="https://scontent-mia3-2.cdninstagram.com/vp/c69486e5aa641d9f4fb389e74087fdfb/5E0FEA31/t51.2885-15/e35/c0.75.877.877a/s240x240/67524515_156209612234747_3747271840880064739_n.jpg?_nc_ht=scontent-mia3-2.cdninstagram.com" class="w-100 pt-4"alt=""></div>
+        <div class="col-4"><img src="https://scontent-mia3-2.cdninstagram.com/vp/c69486e5aa641d9f4fb389e74087fdfb/5E0FEA31/t51.2885-15/e35/c0.75.877.877a/s240x240/67524515_156209612234747_3747271840880064739_n.jpg?_nc_ht=scontent-mia3-2.cdninstagram.com" class="w-100 pt-4"alt=""></div>
     </div>
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,8 +23,15 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                <a class="navbar-brand d-flex" href="{{ url('/') }}">
+                    <!-- {{ config('app.name', 'Laravel') }} -->
+                    <div class="">
+                        <img src="/svg/free-code-camp-logo.svg" style="height:20px; border-right: 1px solid #333" class="pr-3" alt="Free Code Camp Logo">
+                    </div>
+                    <div class="pl-3 pt-1">
+                        freeCodeGram
+                    </div>
+                    
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
